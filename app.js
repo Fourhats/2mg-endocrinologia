0d1dca7840eec900c15c4bcefe5f45f0658b856b
Francisco Cittadini <fran.cittadini@gmail.com>
2016-12-04 22:24:22 -0300

    Added spinner.

--- app.js
+++ app.js
@@ -15,14 +15,12 @@ angular.module("workshopsApp", ['ngRoute'])
     })
 
     .controller("MainController", function ($scope, WorkshopsService, UsersService, InscriptionsService) {
+        $scope.isLoading = false;
         $scope.isDniSet = false;
         $scope.workshops = [];
 
         $scope.submitDni = function (dni) {
-            if (!isNumber(dni)) {
-                alert('DNI no válido');
-                return;
-            }
+            $scope.isLoading = true;
 
             UsersService.userExists(dni).then(function (response) {
                 var userExists = response.data;
@@ -40,22 +38,29 @@ angular.module("workshopsApp", ['ngRoute'])
                     alert('DNI no válido');
                     return;
                 }
-            })
-        }
-
-        function isNumber(s) {
-            var x = +s; //cast
-            return x.toString() === s; //compare
+            }, function (response) {
+                console.log(response.data.error)
+                alert('DNI no válido');
+                return;
+            }).finally(function () {
+                $scope.isLoading = false;
+            });
         }
 
         $scope.getWorkshops = function () {
+            $scope.isLoading = true;
+
             WorkshopsService.getWorkshops($scope.userDni).then(function (response) {
                 $scope.workshops = response.data;
+            }).finally(function () {
+                $scope.isLoading = false;
             });
         }
 
         $scope.enroll = function (workshop) {
             if (confirm("¿Confirmar inscripción a " + workshop.title + "?")) {
+                $scope.isLoading = true;
+
                 var inscription = {
                     workshopId: workshop.id,
                     dni: $scope.userDni
@@ -81,64 +86,36 @@ angular.module("workshopsApp", ['ngRoute'])
         this.getWorkshops = function (dni) {
             var url = "https://thawing-plains-13266.herokuapp.com/workshops/" + dni;
 
-            return $http.get(url).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.get(url);
         };
 
         this.getWorkshop = function (workshopId) {
             var url = "https://thawing-plains-13266.herokuapp.com/workshops/" + workshopId;
 
-            return $http.get(url).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.get(url);
         }
 
         this.createWorkshop = function (workshop) {
-            return $http.post("https://thawing-plains-13266.herokuapp.com/workshops", workshop).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.post("https://thawing-plains-13266.herokuapp.com/workshops", workshop);
         }
 
         this.getInscriptionsReport = function () {
-            return $http.get("https://thawing-plains-13266.herokuapp.com/inscriptions/report").then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.get("https://thawing-plains-13266.herokuapp.com/inscriptions/report");
         }
     })
     .service("UsersService", function ($http) {
         this.userExists = function (dni) {
             var url = "https://thawing-plains-13266.herokuapp.com/users/" + dni;
 
-            return $http.get(url).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.get(url);
         }
 
         this.createUser = function (user) {
-            return $http.post("https://thawing-plains-13266.herokuapp.com/users", user).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.post("https://thawing-plains-13266.herokuapp.com/users", user);
         }
     })
     .service("InscriptionsService", function ($http) {
         this.saveInscription = function (inscription) {
-            return $http.post("https://thawing-plains-13266.herokuapp.com/inscriptions", inscription).then(function (response) {
-                return response;
-            }, function (response) {
-                console.log(response.data.error);
-            });
+            return $http.post("https://thawing-plains-13266.herokuapp.com/inscriptions", inscription)
         }
     });
